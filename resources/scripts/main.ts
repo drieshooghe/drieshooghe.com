aa9540de79efe7699916d279f393462caed00bf5
Dries Hooghe <dries.hooghe@gmail.com>
2018-06-13 19:06:07 +0200

    Cursor animation WIP

---
+++ resources/scripts/main.ts
@@ -0,0 +1,12 @@
+window.onload = function(){
+    let eLines = document.querySelectorAll('.js-typewriting');
+    for (let i=0; i < eLines.length; i++){
+        let ePromptIcon = eLines[i].querySelector('.js-prompt-icon');
+        let ePromptText = eLines[i].querySelector('.js-text');
+        
+    }
+}
+
+// function dispChars (e: element , l: Array<string>) {
+//    return 
+// };
