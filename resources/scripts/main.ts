3314ccebe05f7458ec68ad2e5d47de46aaebd2f6
Dries Hooghe <dries.hooghe@gmail.com>
2018-06-15 11:37:14 +0200

    Finish typewriter effect

--- resources/scripts/main.ts
+++ resources/scripts/main.ts
@@ -1,58 +1,63 @@
 window.onload = function () {
 
-    class Line {
-        icon: Element;
-        pfx: Element;
-        message: Element;
-
-        constructor(icon: Element, pfx: Element, message: Element) {
-            this.icon = icon;
-            this.pfx = pfx;
-            this.message = message;
-        }
-
-        type(): void {
-            function restore(e: Element, i: number) {
-                if (e) {
-                    let sValue: string = e.getAttribute('data-content');
-                    if (i < sValue.length) {
-                        e.innerHTML += sValue[i];
-                        setTimeout(function () { restore(e, i); }, 100);
-                        i++;
-                    }
-                }
-            }
-
-            restore(this.icon, 0);
-            restore(this.pfx, 0);
-            restore(this.message, 0);
-        }
-    }
-
-    function sleep(ms: Number) {
+    function sleep(ms: Number): Promise<void> {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
+    class Text {
+        protected element: Element;
 
-    function dispChars(eLine: Element) {
-        
-        // Get HTML items
-        let promptLine = new Line(
-            eLine.getElementsByClassName('js-prompt-icon')[0],
-            eLine.getElementsByClassName('js-prompt-prefix')[0],
-            eLine.getElementsByClassName('js-prompt-text')[0]
-        );
+        constructor(element: Element) {
+            this.element = element;
+        }
 
-        eLine.classList.remove('hidden');
-        promptLine.type();
+        async type(): Promise<void> {
+            if (!this.element) return;
+            let sValue: string = this.element.getAttribute('data-content');
+
+            for (let i = 0; i < sValue.length; ++i) {
+                await sleep(50);
+                this.element.innerHTML += sValue[i];
+            }
+        }
+
     }
 
-    function displayLines(eLines: HTMLCollection) {
-        for (let i = 0; i < eLines.length; i++) {     
-            dispChars(eLines[i]);
+    class Line {
+        protected icon: Text;
+        protected pfx: Text;
+        protected message: Text;
+
+        constructor(eLine: Element) {
+            this.icon = new Text(eLine.getElementsByClassName('js-prompt-icon')[0]);
+            this.pfx = new Text(eLine.getElementsByClassName('js-prompt-prefix')[0]);
+            this.message = new Text(eLine.getElementsByClassName('js-prompt-text')[0]);
+        }
+
+        async type(): Promise<void> {
+            await this.icon.type();
+            await sleep(400);
+            await this.pfx.type();
+            await this.message.type();
         }
     }
 
-    let eLines = document.getElementsByClassName('js-typewriting');
-    displayLines(eLines);
-}
+    class Lines {
+        protected lines: Line[];
 
+        constructor (lines: HTMLCollection) {
+            this.lines = [];
+            for (let i = 0; i < lines.length; i++) {     
+                this.lines.push(new Line(lines[i]));
+            }
+        }
+
+        async type() {
+            for (const line of this.lines) {
+                await line.type();
+            }
+        }
+    }
+
+    const lines = new Lines(document.getElementsByClassName('js-typewriting'));
+    lines.type().catch(console.error);
+}
